17aee6e0d192bb1a7f44cf62cf68938b3bf4da9f
leesc22 <leesiewching22@gmail.com>
2017-08-23 18:05:49 +0800

    update Query 1 to method

--- app/main.rb
+++ app/main.rb
@@ -3,12 +3,19 @@ require_relative 'models/senator'
 
 
 # Query 1
-puts "Senators:"
-Senator.where(state: "CA").order(:lastname).each do |s|
-  puts "  #{s.firstname} #{s.middlename} #{s.lastname} (#{s.party})".squeeze(" ")
+def list_legislators_by_state(state)
+	puts "Senators:"
+	Senator.where(state: state).order(:lastname).each do |s|
+	  puts "  #{s.firstname} #{s.middlename} #{s.lastname} (#{s.party})".squeeze(" ")
+	end
+
+	puts "Representatives:"
+	Representative.where(state: state).order(:lastname).each do |r|
+		puts "  #{r.firstname} #{r.middlename} #{r.lastname} (#{r.party})".squeeze(" ")
+	end
 end
 
-puts "Representatives:"
-Representative.where(state: "CA").order(:lastname).each do |r|
-	puts "  #{r.firstname} #{r.middlename} #{r.lastname} (#{r.party})".squeeze(" ")
-end
+list_legislators_by_state("CA")
+
+# Query 2
+
