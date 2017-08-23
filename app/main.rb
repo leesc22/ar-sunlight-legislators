e86d5a63e97892d9671446c5714cb71206b5de40
leesc22 <leesiewching22@gmail.com>
2017-08-23 17:59:41 +0800

    complete Query 1

--- app/main.rb
+++ app/main.rb
@@ -1,1 +1,14 @@
+require_relative 'models/representative'
+require_relative 'models/senator'
+
+
 # Query 1
+puts "Senators:"
+Senator.where(state: "CA").order(:lastname).each do |s|
+  puts "  #{s.firstname} #{s.middlename} #{s.lastname} (#{s.party})".squeeze(" ")
+end
+
+puts "Representatives:"
+Representative.where(state: "CA").order(:lastname).each do |r|
+	puts "  #{r.firstname} #{r.middlename} #{r.lastname} (#{r.party})".squeeze(" ")
+end
