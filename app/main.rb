a53b85bcd57365ebbef596472c5dccb32c8a77b8
leesc22 <leesiewching22@gmail.com>
2017-08-23 18:29:37 +0800

    complete Query 2

--- app/main.rb
+++ app/main.rb
@@ -18,4 +18,17 @@ end
 list_legislators_by_state("CA")
 
 # Query 2
+def statistics_active(gender)
+  gender_string = gender == "M" ? "Male" : "Female"
+  senators_active = Senator.where(in_office: true)
+  number_senators = senators_active.where(gender: gender).count
+  percentage_senators = 100 * number_senators / senators_active.count
+  representatives_active = Representative.where(in_office: true)
+  number_represenetatives = representatives_active.where(gender: gender).count
+  percentage_representatives = 100 * number_represenetatives / representatives_active.count
+  puts "#{gender_string} Senators: #{number_senators} (#{percentage_senators}%)"
+  puts "#{gender_string} Representatives: #{number_represenetatives} (#{percentage_representatives}%)"
+end
 
+statistics_active("M")
+statistics_active("F")
